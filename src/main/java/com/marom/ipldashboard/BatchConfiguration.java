711f906fd0dac200c2ad193af0a9f5039d1d8758
marom <maro.muszynski@gmail.com>
2021-05-09 21:12:51 +0100

    create a batch configuration class, add reader, processor and writer beans

---
+++ src/main/java/com/marom/ipldashboard/BatchConfiguration.java
@@ -0,0 +1,68 @@
+package com.marom.ipldashboard;
+
+import com.marom.ipldashboard.data.MatchDataProcessor;
+import com.marom.ipldashboard.data.MatchInput;
+import com.marom.ipldashboard.model.Match;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+    private static final String[] FIELD_NAMES = new String[]{"id","city","date","player_of_match","venue","neutral_venue","team1","team2","toss_winner","toss_decision","winner","result","result_margin","eliminator"
+            ,"method","umpire1","umpire2"};
+
+    public JobBuilderFactory jobBuilderFactory;
+    public StepBuilderFactory stepBuilderFactory;
+    private DataSource dataSource;
+
+    @Autowired
+public BatchConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+    this.jobBuilderFactory = jobBuilderFactory;
+    this.stepBuilderFactory = stepBuilderFactory;
+}
+
+    @Bean
+    public FlatFileItemReader<MatchInput> reader() {
+        return new FlatFileItemReaderBuilder<MatchInput>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("matches.csv"))
+                .delimited()
+                .names(FIELD_NAMES)
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {{
+                    setTargetType(MatchInput.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    public MatchDataProcessor processor() {
+        return new MatchDataProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
+        this.dataSource = dataSource;
+        return new JdbcBatchItemWriterBuilder<Match>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO match (id,city,date,player_of_match,venue,team1,team2,toss_winner,toss_decision,match_winner,result,result_margin,umpire1,umpire2) " +
+                        "VALUES (:id,:city,:date,:playerOfMatch,:venue,:team1,:team2,:tossWinner,:tossDecision,:matchWinner,:result,:resultMargin,:umpire1,:umpire2)")
+                .dataSource(dataSource)
+                .build();
+    }
+
+}
